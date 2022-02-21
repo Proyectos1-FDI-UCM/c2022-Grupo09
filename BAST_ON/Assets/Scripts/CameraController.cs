80700d31715435c07146d65781c2c6d4729a4c9a
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2022-02-21 16:50:37 +0100

    Colliders de mi escena, prefab player y cámara que no funca

---
+++ BAST_ON/Assets/Scripts/CameraController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    ///<summary>
+    ///Referencia al jugador
+    ///</summary>
+    [SerializeField] private GameObject _player;
+    ///<summary>
+    ///Vector que desplaza el centro de la cámara.
+    ///</summary>
+    [SerializeField] private Vector3 _offset = new Vector3(1, 0, 0);
+    ///<summary>
+    ///Valor que determina a qué distancia se queda la cámara del punto final
+    ///</summary>
+    [SerializeField] private float _lerpSpeed = 0.5f;
+    
+    ///<summary>
+    ///Referencia al transform del Player
+    ///</summary>
+    private Transform _playerTransform;
+    ///<summary>
+    ///Referencia al transform de la cámara
+    ///</summary>
+    private Transform _cameraTransform;
+    ///<summary>
+    ///Valor interno para el cálculo de las posiciones
+    ///</summary>
+    private Vector3 _nextPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Se rellenan las referencias a los Transform
+        _playerTransform = _player.GetComponent<Transform>();
+        _cameraTransform = this.gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    
+    private void Update() {
+        _nextPosition = Vector3.Lerp(_cameraTransform.position, _playerTransform.position + _offset, _lerpSpeed);
+    }
+    void LateUpdate()
+    {
+        //Actualiza el movimiento de la cam.
+        _cameraTransform.position = _nextPosition;
+        
+    }
+}
