39df1c4e7a3bbc7df6147a32f8e5ba8d7c7f6925
MaikCG <micurros@ucm.es>
2022-03-01 20:16:03 +0100

    No pegar al abajo si estás en el suelo

---
+++ BAST_ON/Assets/Scripts/Player/FloorDetector.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorDetector : MonoBehaviour
+{
+    #region references
+    [SerializeField]
+    private GameObject _player;
+    private CharacterAttackController _AttackController;
+    #endregion
+    #region methods
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _AttackController.SetFloorDetector(true);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _AttackController.SetFloorDetector(false);
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _AttackController = _player.GetComponent<CharacterAttackController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
