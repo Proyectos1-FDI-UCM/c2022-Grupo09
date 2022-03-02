d29862d8bc912b24d747de9d815d71353b0b79a5
dlbcb <diegol08@ucm.es>
2022-03-02 09:47:00 +0100

    Vida y daño 0.5

--- BAST_ON/Assets/Scripts/Character_HealthManager.cs
+++ BAST_ON/Assets/Scripts/Character_HealthManager.cs
@@ -5,6 +5,11 @@ using UnityEngine.UI;
 
 public class Character_HealthManager : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject _myPlayer;
+
+    private EnemyLifeComponent _myEnemyLifeComponent;
     ///<summary>
     ///Vida maxima que puede tener el jugador
     ///</summary>
@@ -14,6 +19,8 @@ public class Character_HealthManager : MonoBehaviour
     ///Valor que determina la vida máxima
     ///</summary>
     [SerializeField] private int _currentHealth;
+
+    [SerializeField] private int _hitDamage;
 
     ///<summary>
     ///Array que determina la vida en el UI
@@ -51,7 +58,7 @@ public class Character_HealthManager : MonoBehaviour
        _currentHealth += value;
        if(_currentHealth <= 0)
        {
-           //Administrar muerte del jugador
+            Die();
        }
        if(_currentHealth > _maxHealth)
        {
@@ -59,6 +66,25 @@ public class Character_HealthManager : MonoBehaviour
            _currentHealth = _maxHealth;
        }
        updateLifeBar();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Si se produce colisión entre enemigos baja la vida
+        EnemyLifeComponent enemy = collision.gameObject.GetComponent<EnemyLifeComponent>();
+        if (enemy != null)
+        {
+            Damage();
+        }
+    }
+
+    public void Damage()
+    {
+        _currentHealth -= _hitDamage;
+    }
+
+    public void Die()
+    {
+        Destroy(_myPlayer);
     }
 
 
