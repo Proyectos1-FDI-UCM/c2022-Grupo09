b21bc6577cc275dc1c8f9cb4c69787d3c8b75e78
dlbcb <diegol08@ucm.es>
2022-03-04 18:17:43 +0100

    Movimiento disparo

--- BAST_ON/Assets/Scripts/Enemy/DisparadorController.cs
+++ BAST_ON/Assets/Scripts/Enemy/DisparadorController.cs
@@ -6,9 +6,8 @@ public class DisparadorController : MonoBehaviour
 {
     #region parameters
     [SerializeField]
-    private GameObject _myDisp, _myPlayer, _myEnemy;
+    private GameObject _myDisp;
     private GameObject _myInstance;
-    private Vector3 pos;
     [SerializeField]
     private float frecuencia=6;
     private float timer;
@@ -22,12 +21,10 @@ public class DisparadorController : MonoBehaviour
 
     void Update()
     {
-        pos = _myPlayer.transform.position - _myEnemy.transform.position;
         timer += Time.deltaTime;
             if (timer >= frecuencia)
             {
             _myInstance = Instantiate(_myDisp, transform.position, Quaternion.identity);
-            _myInstance.transform.Rotate(pos);
             timer = 0;
             }
             
--- BAST_ON/Assets/Scripts/Enemy/EnemyShot.cs
+++ BAST_ON/Assets/Scripts/Enemy/EnemyShot.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class EnemyShot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject _myPlayer;
+    [SerializeField]
+    private float _speed;
+    private Vector2 objtv;
+    private Vector2 rotation;
     void Start()
     {
-        
+        objtv = new Vector2(_myPlayer.transform.position.x, _myPlayer.transform.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+        transform.position = Vector2.MoveTowards(transform.position, objtv, _speed * Time.deltaTime);
     }
 }
