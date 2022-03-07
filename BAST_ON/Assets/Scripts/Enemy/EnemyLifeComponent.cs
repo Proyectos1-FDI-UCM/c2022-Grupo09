d4c32ca4457890e859b7e51e58bf8fe46f466d2d
luisja30 <luisjnav@ucm.es>
2022-03-07 20:32:30 +0100

    Daño de enemigo e interruptor gameobject

--- BAST_ON/Assets/Scripts/Enemy/EnemyLifeComponent.cs
+++ BAST_ON/Assets/Scripts/Enemy/EnemyLifeComponent.cs
@@ -6,13 +6,18 @@ public class EnemyLifeComponent : MonoBehaviour
 {
     #region references 
     [SerializeField]
-    private int _currentHealth = 1;
+    private int _currentHealth = 3;
 
     [SerializeField]
-    private int _maxHealth = 1;
+    private int _maxHealth = 3;
 
     [SerializeField]
     private GameObject _myEnemy;
+
+    [SerializeField]
+    private GameObject _myPowerUp;
+
+    
 
     #endregion
 
@@ -24,7 +29,9 @@ public class EnemyLifeComponent : MonoBehaviour
 
         if (_currentHealth <= 0)
         {
+            ReleasePowerUp();
             Die();
+           
         }
         if (_currentHealth > _maxHealth)
         {
@@ -38,12 +45,15 @@ public class EnemyLifeComponent : MonoBehaviour
         Destroy(_myEnemy);
     }
 
-   
+    public void ReleasePowerUp()
+    {
+        Instantiate(_myPowerUp, _myEnemy.transform);
+    }
     #endregion
     // Start is called before the first frame update
     void Start()
     {
-        
+        _myPowerUp = gameObject;
     }
 
     // Update is called once per frame
