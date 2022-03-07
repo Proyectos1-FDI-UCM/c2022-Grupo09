4c3398df53b068dc7a4e78bdfc601153533a5643
MaikCG <micurros@ucm.es>
2022-03-07 18:58:45 +0100

    Crado el método PinchosDamageController para Juani

---
+++ BAST_ON/Assets/Scripts/Enemy/PinchosDamageController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinchosDamageController : MonoBehaviour
+{
+    #region parameters
+    [SerializeField]
+    private int _damage = 1;
+    #endregion
+    
+    #region methods
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Character_HealthManager player = collision.GetComponent<Character_HealthManager>();
+        if (player != null)
+        {
+            player.ChangeHealthValue(_damage);
+        }
+    }
+    #endregion
+}
