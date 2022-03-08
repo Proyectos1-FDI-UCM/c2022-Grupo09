6baa2ffe6e42792ce7f41ac24cdb65448ba74b6a
MaikCG <micurros@ucm.es>
2022-03-08 14:39:03 +0100

    Interruptores y puertas terminados

--- BAST_ON/Assets/Scripts/SwitchController.cs
+++ BAST_ON/Assets/Scripts/SwitchController.cs
@@ -5,27 +5,39 @@ using UnityEngine;
 public class SwitchController : MonoBehaviour
 {
     #region references
-
     [SerializeField]
     private GameObject _myDoor;
+    private BoxCollider2D _doorCollider;
+    private Animator _switchAnimator;
+    private Animator _doorAnimator;
+    #endregion
 
-    public Animator _switch;
-    public Animator _door;
+    #region properties
+    private bool _opened = false;
     #endregion
 
     #region methods
-    private void OnTriggerEnter(Collider collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        _switch.SetBool("ON", true);
-        _door.SetBool("OPEN", true);
+        BastonImpulseController baston = collision.GetComponent<BastonImpulseController>();
+        EnemyShot disparo = collision.GetComponent<EnemyShot>();
+        if (baston != null || disparo != null)
+        {
+            _opened = !_opened;
+            _doorCollider.enabled = !_opened;
+            _switchAnimator.SetBool("ON", _opened);
+            _doorAnimator.SetBool("OPEN", _opened);
+        }
     }
     #endregion
     // Start is called before the first frame update
     void Start()
     {
-        _switch.SetBool("ON", false);
-        _door.SetBool("OPEN", false);
-
+        _doorCollider = _myDoor.GetComponent<BoxCollider2D>();
+        _switchAnimator = GetComponent<Animator>();
+        _doorAnimator = _myDoor.GetComponent<Animator>();
+        _switchAnimator.SetBool("ON", false);
+        _doorAnimator.SetBool("OPEN", false);
     }
 }
 
