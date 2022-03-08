a02ee52df716837bd781aae0f8cb543271b8a912
MaikCG <micurros@ucm.es>
2022-03-08 21:04:32 +0100

    ENEMIGOS DROPEAN FRUTA

--- BAST_ON/Assets/Scripts/Enemy/EnemyLifeComponent.cs
+++ BAST_ON/Assets/Scripts/Enemy/EnemyLifeComponent.cs
@@ -7,6 +7,8 @@ public class EnemyLifeComponent : MonoBehaviour
     #region parameters
     [SerializeField]
     private int _maxHealth = 3;
+    [SerializeField]
+    private int _dropPercentage = 50;
     #endregion
 
     #region properties
@@ -71,7 +73,7 @@ public class EnemyLifeComponent : MonoBehaviour
     
     public void ReleasePowerUp()
     {
-        Instantiate(_myPowerUp, _thisTransform);
+        if (Random.Range(0, (100 / _dropPercentage) - 1) == 0)Instantiate(_myPowerUp, _thisTransform.position, Quaternion.identity);
     }
     #endregion
     // Start is called before the first frame update
