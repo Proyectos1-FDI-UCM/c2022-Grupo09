1d488248c78a02381e7ca8b5ece92924b3030d1a
MaikCG <micurros@ucm.es>
2022-03-12 20:41:49 +0100

    corrección

--- BAST_ON/Assets/Scripts/Player/CharacterAttackController.cs
+++ BAST_ON/Assets/Scripts/Player/CharacterAttackController.cs
@@ -77,7 +77,7 @@ public class CharacterAttackController : MonoBehaviour
             if (_myWallDetector.isInWall())
             {
                 if (_defaultDirection > 0) _bastonTransform.Rotate(Vector3.forward, -60);
-                else _bastonTransform.Rotate(Vector3.forward, -150);
+                else _bastonTransform.Rotate(Vector3.forward, -120);
             }
             // Si se ha escogido una direcci�n para el ataque
             else if (horizontalAttackDirection != 0 || verticalAttackDirection != 0)
