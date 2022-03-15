bbe439d2148e35b8ce6ea0344679a635bc9f9813
luisja30 <luisjnav@ucm.es>
2022-03-16 00:25:10 +0100

    intento fallido de hacer el enemigo escalador (al final lo sacaré srry)

--- BAST_ON/Assets/Scripts/Enemy/EscalatorEnemy.cs
+++ BAST_ON/Assets/Scripts/Enemy/EscalatorEnemy.cs
@@ -9,22 +9,20 @@ public class EscalatorEnemy : MonoBehaviour
     [SerializeField]
     private bool rightMov = true;
     [SerializeField]
-    private GameObject _limIzq, _limDer, _limUpLeft, _limUpRight;
+    private GameObject _limIzq, _limDer, _limUpIz, _limUpDer;
 
     void Update()
     {
-
         if (rightMov)
         {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
+
             if (transform.position.x >= _limDer.transform.position.x)
             {
                 transform.eulerAngles = new Vector3(0, -180, 0);
             }
             else if (transform.position.x <= _limIzq.transform.position.x)
-
             {
-
                 transform.eulerAngles = new Vector3(0, 0, 0);
             }
         }
@@ -38,10 +36,34 @@ public class EscalatorEnemy : MonoBehaviour
             }
             else if (transform.position.x >= _limDer.transform.position.x)
             {
-
                 transform.eulerAngles = new Vector3(0, 0, 0);
             }
+        }
+        else if (transform.position.x == _limDer.transform.position.x)
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+            
+            if (transform.position.y <= _limUpDer.transform.position.y)
+            {
+                transform.eulerAngles = new Vector3(0, 0, -90);
+            }
+            else if (transform.position.y >= _limUpDer.transform.position.y)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+        }
+        else if (transform.position.x == _limIzq.transform.position.x)
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
 
+            if (transform.position.y <= _limUpIz.transform.position.y)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            else if (transform.position.y >= _limUpIz.transform.position.y)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+            }
 
         }
     }
