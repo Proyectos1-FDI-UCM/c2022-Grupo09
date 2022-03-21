8c01c16b39880af0a8ae13c1d8b5dc643281ab7a
MaikCG <micurros@ucm.es>
2022-03-21 19:09:51 +0100

    Enemigo se frena tras x tiempo después de un golpe
    Also, añadida la referencia al player a los enemigos del nivel 2 que daban error

--- BAST_ON/Assets/Scripts/Enemy/EnemyStrikingForceController.cs
+++ BAST_ON/Assets/Scripts/Enemy/EnemyStrikingForceController.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class EnemyStrikingForceController : MonoBehaviour
 {
+    #region parameters
     [SerializeField] private float conversionValue = 1.0f;
+    [SerializeField] private float _impulseStopTime = 3.0f;
+    #endregion
+
+    #region properties
+    private bool hasBeenStruck = false;
+    private float _impulsedElapsedTime = 0;
+    #endregion
+
+    #region references
     private EnemyPatrulla _myEnemyPatrulla;
     private EnemyLifeComponent _myEnemyLifeComponent;
 
     private Animator _myAnimator;
     private Transform _myTransform;
     private Rigidbody2D _myRigidBody;
-    private bool hasBeenStruck = false;
+    #endregion
+
+    #region methods
     public void StrikeCallback(Vector3 strikeVector){
         _myEnemyPatrulla.enabled = false;
         _myRigidBody.WakeUp();
@@ -19,7 +31,10 @@ public class EnemyStrikingForceController : MonoBehaviour
         _myRigidBody.AddForce(strikeVector, ForceMode2D.Impulse);
         hasBeenStruck = true;
         _myAnimator.SetBool("haSidoGolpeado", true);
+
+        _impulsedElapsedTime = 0;
     }
+
     private void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("la colisión se está detectando");
@@ -36,14 +51,33 @@ public class EnemyStrikingForceController : MonoBehaviour
             _myEnemyPatrulla.enabled = true;
         }
     }
-    
-    
-        private void Start() 
-        {
+
+    private void RestartEnemy()
+    {
+        _myRigidBody.velocity = Vector3.zero;
+        hasBeenStruck = false;
+        _myAnimator.SetBool("haSidoGolpeado", false);
+
+        // Llamar aquí al método de restart ruta y que al llegar al sitio reactive el comp de patrulla
+        // ¿Que ese método ha una cosa u otra dependiendo del enemigo con un enum?
+    }
+    #endregion
+
+    private void Start() 
+    {
         _myAnimator = GetComponent<Animator>();
         _myEnemyLifeComponent = GetComponent<EnemyLifeComponent>();
         _myEnemyPatrulla = GetComponent<EnemyPatrulla>();
         _myRigidBody = GetComponent<Rigidbody2D>();
         _myTransform = transform;
-        }    
+    }
+
+    private void Update()
+    {
+        if (hasBeenStruck && _impulsedElapsedTime > _impulseStopTime)
+        {
+            RestartEnemy();
+        }
+        else if (hasBeenStruck) _impulsedElapsedTime += Time.deltaTime;
+    }
 }
