e627c60c9848d8fba6fed391435409e206e27bf8
MiguelCGa <micurros@ucm.es>
2022-03-23 18:52:23 +0100

    Eso que creía que había hecho rompía todo, un saludo
    Ya no y funciona regular lo que pretendía hacer

--- BAST_ON/Assets/Scripts/Enemy/EnemyPatrulla.cs
+++ BAST_ON/Assets/Scripts/Enemy/EnemyPatrulla.cs
@@ -30,10 +30,10 @@ public class EnemyPatrulla : MonoBehaviour
         _targetPosition = _rightTarget;
     }
 
-    void Update()
+    /*void Update()
     {   
         
-        /*if (rightMov)
+        if (rightMov)
         {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
             if (transform.position.x >= _limDer.transform.position.x)
@@ -61,17 +61,21 @@ public class EnemyPatrulla : MonoBehaviour
             }
 
 
-        }*/
+        }
+    }*/
 
+    private void FixedUpdate()
+    {
         _movementDirection = _targetPosition - _myTransform.position;
 
-        _myRigidbody.MovePosition(_movementDirection * speed * Time.deltaTime);
+        _myRigidbody.MovePosition((Vector3)_myRigidbody.position + _movementDirection * speed * Time.fixedDeltaTime);
 
         _myTransform.rotation = Quaternion.identity;
         if (_movementDirection.x < 0) _myTransform.Rotate(Vector2.up, 180);
 
-        if (_myTransform.position == _targetPosition && _targetPosition == _rightTarget) _targetPosition = _leftTarget;
-        else if (_myTransform.position == _targetPosition) _targetPosition = _rightTarget;
+        if (_myTransform.position == _targetPosition)
+            if (_targetPosition == _rightTarget) _targetPosition = _leftTarget;
+            else _targetPosition = _leftTarget;
     }
 
 }
