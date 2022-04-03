76129418100ec73c07625a7329a3022526733bc5
juanixd <jmarelli@ucm.es>
2022-04-03 17:52:34 +0200

    salidas de los niveles prefab y script

---
+++ BAST_ON/Assets/Scripts/salidasyentradasniveles.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class salidasyentradasniveles : MonoBehaviour
+{
+
+    [SerializeField] private GameObject salidanivel;
+    private GameObject player;
+   
+       
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Character_HealthManager player = collision.GetComponent<Character_HealthManager>();
+        
+        if(player != null)
+        {
+            player.transform.position = salidanivel.transform.position;
+        }
+    }
+}
