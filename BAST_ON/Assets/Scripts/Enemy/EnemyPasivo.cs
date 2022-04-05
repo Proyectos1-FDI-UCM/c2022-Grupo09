6bf1f21106b7407742c112cfb37edd34cc3a75f4
dlbcb <diegol08@ucm.es>
2022-04-05 22:40:39 +0200

    Revert "Revert "Pasivo""
    This reverts commit 08844de40c92286e3293240f640d9fa1e43e52b2.

--- BAST_ON/Assets/Scripts/Enemy/EnemyPasivo.cs
+++ BAST_ON/Assets/Scripts/Enemy/EnemyPasivo.cs
@@ -6,106 +6,53 @@ public class EnemyPasivo : MonoBehaviour
 {
     #region parameters
     [SerializeField]
-    private float speed = 3f, detectdist = 1f;
-    /// <summary>
-    /// Determina si un enemigo se quedar� quieto en un punto.
-    /// En caso de estar en otro sitio, se mover� por defecto al l�mite derecho de la patrulla.
-    /// </summary>
-    private bool rotated = false, wallInfo;
-    int layerF;
+    private float speed = 3, distance = 1;
+    private bool rotate = true;
     #endregion
 
     #region references
     [SerializeField]
-    private GameObject _detectorD, DetectorI;
-    private SpriteRenderer _mySpriteRenderer;
-    private Rigidbody2D _myRigidbody;
-    private EnemyLifeComponent _myLifeComponent;
-    //[SerializeField] private Transform _detector;
+    private Transform _detector;
+    private Rigidbody2D _myRigidBody;
+    private RaycastHit2D _groundDetect, _wallDetect;
     #endregion
 
     #region properties
-    private float _originalSpeed;
-    private Vector2 _movementDirection, _detectorOrigin;
-    private Transform _myDetectorD, _myDetectorI;
-    private RaycastHit2D _wallInfoD, _wallInfoI;
-    private RaycastHit2D _floorInfo;
-
-    private Vector2 _gravity;
-    private float _onAirElapsedTime = 0;
+    private Vector2 _movementDirection;
     #endregion
 
     #region methods
-    public void SlowDown(float speedReducer)
-    {
-        speed = _originalSpeed / speedReducer;
-    }
-    public void RestoreSpeed()
-    {
-        speed = _originalSpeed;
-    }
+
     #endregion
 
 
     private void Start()
     {
-        layerF = LayerMask.GetMask("Floor");
-        _mySpriteRenderer = GetComponent<SpriteRenderer>();
-        _myRigidbody = GetComponent<Rigidbody2D>();
-        _myLifeComponent = GetComponent<EnemyLifeComponent>();
-
-        _myDetectorD = _detectorD.transform;
-
+        _myRigidBody = GetComponent<Rigidbody2D>();
         _movementDirection = Vector2.right;
-        _originalSpeed = speed;
     }
 
     private void Update()
     {
-        // Ajuste de la rotaci�n del sprite del enemigo en funci�n de la direcci�n
-        // Si el movimiento es hacia la izquierda lo gira
-        _mySpriteRenderer.flipX = _movementDirection.x < 0;
-        if (!rotated)
+        _myRigidBody.MovePosition(_movementDirection);
+        _groundDetect = Physics2D.Raycast(_detector.position, Vector2.down, distance);
+        _wallDetect = Physics2D.Raycast(_detector.position, _movementDirection, distance);
+
+        if (!_groundDetect.collider || _wallDetect.collider)
         {
-            _detectorOrigin = _myDetectorD.position;
-            _wallInfoD = Physics2D.Raycast(_detectorOrigin, Vector2.right, detectdist);
+            if (rotate)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                rotate = !rotate;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                rotate = !rotate;
+            }
         }
-        else
-        {
-            _detectorOrigin = _myDetectorI.position;
-            _wallInfoI = Physics2D.Raycast(_detectorOrigin, Vector2.left, detectdist);
-        }
 
-
-        _floorInfo = Physics2D.Raycast(_detectorOrigin, Vector2.down, detectdist);
-
-        if (!rotated) wallInfo = _wallInfoD.collider;
-        else wallInfo = _wallInfoI.collider;
-
-        if (!wallInfo || _floorInfo.collider)
-        {
-            _movementDirection = -_movementDirection;
-            rotated = !rotated;
-        }
     }
 
-    private void FixedUpdate()
-    {
-        if (!_floorInfo) _onAirElapsedTime += Time.deltaTime;
-        else _onAirElapsedTime = 0;
-
-        
-
-        // C�lculo de la gravedad
-        _gravity = (Vector2.down * _myRigidbody.gravityScale * _onAirElapsedTime);
-
-        // Llamada el m�todo del Life Component que recibe la direcci�n del movimiento
-        _myLifeComponent.SetMovementDirection(_movementDirection);
-
-        // Aplicaci�n del movimiento
-        _myRigidbody.MovePosition(_myRigidbody.position + _gravity + _movementDirection.normalized * speed * Time.fixedDeltaTime);
-
-        
-    }
 
 }
