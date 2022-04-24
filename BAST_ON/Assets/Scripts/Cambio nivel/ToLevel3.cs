5839104866249328b2396d3fc7718e3e0e97be3f
MiguelCGa <micurros@ucm.es>
2022-04-24 20:34:06 +0200

    Escenas de niveles separadas

---
+++ BAST_ON/Assets/Scripts/Cambio nivel/ToLevel3.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToLevel3 : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Character_HealthManager player = collision.GetComponent<Character_HealthManager>();
+        if (player != null)
+        {
+            PlayerPrefs.DeleteAll();
+            SceneManager.LoadScene("Lvl 3");
+        }
+    }
+}
